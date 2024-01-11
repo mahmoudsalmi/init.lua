fb5bb4d3e9ffa926ddae58f6aad7a767a638af33
Mahmoud SALMI <mahmoud@salmi.dev>
2024-01-11 23:37:19 +0100

    add some config + some reorg

--- lua/salmidev/init.lua
+++ lua/salmidev/init.lua
@@ -1,3 +1,34 @@
 require("salmidev.set")
 require("salmidev.remap")
 require("salmidev.lazy-init")
+
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+
+function R(name)
+  require("plenary.reload").reload_module(name)
+end
+
+local yank_group = augroup('HighlightYank', {})
+autocmd('TextYankPost', {
+  group = yank_group,
+  pattern = '*',
+  callback = function()
+    vim.highlight.on_yank({
+      higroup = 'IncSearch',
+      timeout = 60,
+    })
+  end,
+})
+
+-- local SalmiDevGroup = augroup('SalmiDev', {})
+-- autocmd({"BufWritePre"}, {
+--   group = SalmiDevGroup,
+--   pattern = "*",
+--   command = [[%s/\s\+$//e]],
+-- })
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
+
