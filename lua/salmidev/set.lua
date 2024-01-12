4272b952f911c6bbd2a8ce2e32874ba291a95945
Mahmoud SALMI <mahmoud@salmi.dev>
2024-01-12 02:49:46 +0100

    change undodir path

--- lua/salmidev/set.lua
+++ lua/salmidev/set.lua
@@ -1,3 +1,7 @@
+local _, os_name = require("salmidev.utils").os_name()
+if os_name == "nixos" then
+  vim.opt.shell = "/run/current-system/sw/bin/zsh"
+end
 -- vim.opt.guicursor = ""
 
 vim.opt.nu = true
@@ -15,7 +19,7 @@ vim.opt.wrap = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undodir = os.getenv("HOME") .. "/.local/share/nvim/undodir"
 vim.opt.undofile = true
 
 vim.opt.hlsearch = false
