38f3fbca8f4405af719591caec587952f586e4ce
Mahmoud SALMI <mahmoud@salmi.dev>
2024-01-15 02:19:16 +0100

    lsp config

--- lua/salmidev/lazy/lsp.lua
+++ lua/salmidev/lazy/lsp.lua
@@ -4,9 +4,45 @@ return {
     "williamboman/mason.nvim",
     "williamboman/mason-lspconfig.nvim",
     "j-hui/fidget.nvim",
+    'hrsh7th/nvim-cmp',
+    'neovim/nvim-lspconfig',
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-path',
+    'hrsh7th/cmp-cmdline',
+    'L3MON4D3/LuaSnip',
+    'saadparwaiz1/cmp_luasnip'
   },
   config = function()
-    require("mason").setup()
+    local cmp = require("cmp")
+    local cmp_select = {behavior = cmp.SelectBehavior.Select}
+    local lspconfig = require("lspconfig")
+
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          require('luasnip').lsp_expand(args.body)
+        end,
+      },
+      mapping = cmp.mapping.preset.insert({
+        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+        ["<C-Space>"] = cmp.mapping.complete(),
+      }),
+      sources = cmp.config.sources({
+          { name = 'nvim_lsp' },
+          { name = 'luasnip' },
+        }, {
+          { name = 'buffer' },
+        }
+      )
+    })
+
+    require("fidget").setup()
+    require("mason").setup({
+      PATH = "skip",
+    })
     require("mason-lspconfig").setup({
       ensure_installed = {
         'tsserver',
@@ -15,17 +51,26 @@ return {
       },
       handlers = {
         function(server_name)
-          local lsp_server = require("lspconfig")[server_name]
-          local opts = {}
-
-          if server_name == 'rust_analyzer' then
-            if vim.loop.os_uname().version:lower():match('nixos') then
-              opts.cmd = { os.getenv('HOME') .. '/.nix-profile/bin/rust-analyzer' }
-            end
-          end
-          lsp_server.setup(opts)
+          local opts = {
+            capabilities = require("cmp_nvim_lsp").default_capabilities()
+          }
+          lspconfig[server_name].setup(opts)
         end,
-      }
+        ["lua_ls"] = function ()
+          lspconfig.lua_ls.setup {
+            capabilities = require("cmp_nvim_lsp").default_capabilities(),
+            settings = {
+              Lua = {
+                diagnostics = {
+                  globals = { "vim" }
+                }
+              }
+            }
+          }
+        end,
+      },
     })
+
+    vim.diagnostic.config({ virtual_text = true })
   end
 }
---
+++ lua/salmidev/lazy/null-ls.lua
@@ -0,0 +1,14 @@
+return {
+-- TODO:
+--	"jay-babu/mason-null-ls.nvim",
+--	event = { "BufReadPre", "BufNewFile" },
+--	dependencies = {
+--		"williamboman/mason.nvim",
+--		"nvimtools/none-ls.nvim",
+--	},
+--	config = function()
+--		require("mason-null-ls").setup({
+--			handlers = {},
+--		})
+--	end,
+}
