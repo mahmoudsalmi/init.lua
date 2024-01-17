d092b99443a563d3636aa91de6a62010fc0a0b61
Mahmoud SALMI <mahmoud@salmi.dev>
2024-01-17 23:10:33 +0100

    some changes

--- lua/salmidev/lazy/comments.lua
+++ lua/salmidev/lazy/comments.lua
@@ -2,6 +2,8 @@ return {
   "folke/todo-comments.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
   opts = {
+    sign_priority = 2,
+    -- TODO: asd
     -- your configuration comes here
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
