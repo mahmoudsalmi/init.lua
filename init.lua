312a2542bf150d27126f68546da43464f82ed2bc
Mahmoud SALMI <mahmoud@salmi.dev>
2024-10-30 20:11:22 +0100

    add project local config

--- init.lua
+++ init.lua
@@ -1,1 +1,12 @@
 require("salmidev")
+
+local project_config = vim.fn.getcwd() .. "/.nvimrc"
+if vim.fn.filereadable(project_config) == 1 then
+  vim.cmd("source " .. project_config)
+end
+
+
+local project_config_lua = vim.fn.getcwd() .. "/.nvimrc.lua"
+if vim.fn.filereadable(project_config_lua) == 1 then
+  dofile(project_config_lua)
+end
